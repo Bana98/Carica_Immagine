ab5429f3a6d1652a67361e11b3884a2984ca8fed
Albana <albana@prova.it>
2016-07-01 12:39:18 +0200

    Aggiunti tanti commenti

--- src/ImageFileFilterImageScale.java
+++ src/ImageFileFilterImageScale.java
@@ -15,78 +15,86 @@ import javax.swing.JPanel;
 
 public class ImageFileFilterImageScale extends JFrame implements ActionListener 
 {
-	Image img;
+	Image img;  //l'immagine che voglio visualizzare
 
-	JButton getPictureButton  = new JButton("Get Picture");
+	JButton getPictureButton  = new JButton("Get Picture");  //bottone che quando viene cliccato apre un fileChooser
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {  //MAIN
 		new ImageFileFilterImageScale();
 	}
 
-	public ImageFileFilterImageScale() {
+	public ImageFileFilterImageScale() //costruttore
+	{
 		this.setSize(300, 300);
+		this.setLocation(300, 300);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		JPanel picPanel = new PicturePanel();
-		this.add(picPanel, BorderLayout.CENTER);
+		JPanel picPanel = new PicturePanel();  //creazione picture panel (istanza della classe sotto)
+		this.add(picPanel, BorderLayout.CENTER);  //aggiunge al frame il picture panel. Usa il BorderLayout (io ho sempre usato il FormatLayout, lo creo io il Layout)
 
-		JPanel buttonPanel = new JPanel();
-		getPictureButton.addActionListener(this);
-		buttonPanel.add(getPictureButton);
-		this.add(buttonPanel, BorderLayout.SOUTH);
+		JPanel buttonPanel = new JPanel();  //crea il "contenitore"
+		getPictureButton.addActionListener(this);   //aggiunge l'evento al bottone creato sopra
+		buttonPanel.add(getPictureButton);   //al contenitore viene associato il bottone, quindi ora il bottone sarebbe buttonPanel
+		this.add(buttonPanel, BorderLayout.SOUTH);  //aggiunge alla finestra il bottone, a sud del Layout scelto
 
 		this.setVisible(true);
 	}
 
-	public void actionPerformed(ActionEvent e) {
-		String file = getImageFile();
-		if (file != null) {
-			Toolkit kit = Toolkit.getDefaultToolkit();
-			img = kit.getImage(file);
-			//      img = img.getScaledInstance(300, -1, Image.SCALE_SMOOTH);
-			//      this.repaint();
+	public void actionPerformed(ActionEvent e) //evento quando premuto il bottone
+	{
+		String file = getImageFile();  //in una stringa mette il file immagine che si sceglierà (è il metodo sotto)
+		if (file != null) //se il file è diverso da null
+		{
+			Toolkit kit = Toolkit.getDefaultToolkit();  //serve per di caricare l'immagine
+			img = kit.getImage(file);  //la mia immagine sarà data dal kit + il get dell'immagine selezionata (il file selezionato)
 		}
 
-		ImageIcon icon = new ImageIcon(img);
-
-		JLabel labelImage = new JLabel(icon);
-		add(labelImage);
-		revalidate();
+		ImageIcon icon = new ImageIcon(img);  //crea un ImageIcon nel quale mette l'immagine
+		JLabel labelImage = new JLabel(icon); //crea un label nel quale mette l'icona (nell'icona ci avavo messo l'immagine in questione)
+		add(labelImage);   //aggiunge al frame il label 
+		revalidate();  //con questi due metodi della classe JFrame fa il refresh del frame, sennò l'immagine non appare (revalidate e repaint)
 		repaint();
 	}
 
-	private String getImageFile() {
+	private String getImageFile()  //metodo per scegliere l'immagine che si vuole visualizzare
+	{
 		JFileChooser fc = new JFileChooser();
-		fc.setFileFilter(new ImageFilter());
-		int result = fc.showOpenDialog(null);
-		File file = null;
-		if (result == JFileChooser.APPROVE_OPTION) {
-			file = fc.getSelectedFile();
-			return file.getPath();
+		fc.setFileFilter(new ImageFilter());  //The file filter is used by the file chooser to filter out files from the user's view. ImageFilter è la classe sotto, dice quale tipo di file devi scegliere
+		int result = fc.showOpenDialog(null);  //mostra la finestra che serve per aprire il file selezionato
+		File file = null;  //dichiara un file 
+		if (result == JFileChooser.APPROVE_OPTION) //approva la scelta
+		{
+			file = fc.getSelectedFile();  //nel file ci mette il file selezionato dall'utente
+			return file.getPath();  //ritorna il percorso del file (per aprirlo presumo)
 		} else
 			return null;
 
 	}
 
-	class PicturePanel extends JPanel {
-		public void paint(Graphics g) {
+	class PicturePanel extends JPanel //crea la classe usata sopra che serve effettivamente per stampare/disegnare il tutto nella finestra
+	{
+		public void paint(Graphics g)
+		{
 			g.drawImage(img, 0, 0, this);
 		}
 	}
 
 }
-class ImageFilter extends javax.swing.filechooser.FileFilter {
-	public boolean accept(File f) {
-		if (f.isDirectory())
-			return true;
-		String name = f.getName();
-		if (name.matches(".*((.jpg)|(.gif)|(.png))"))
-			return true;
-		else
-			return false;
+	class ImageFilter extends javax.swing.filechooser.FileFilter 
+	{
+		public boolean accept(File f) 
+		{
+			if (f.isDirectory())
+				return true;
+			String name = f.getName(); //name è il nome del file selezionato dall'utente
+			if (name.matches(".*((.jpg)|(.gif)|(.png))"))  //il file selezionato deve avere queste estensioni
+				return true;  //se il nome del file + l'estensione sono ok, allora ritorna "il file"
+			else
+				return false;  //sennò non ritorna un fico secco in un bel tubo di niente
+		}
+	
+		public String getDescription() 
+		{
+			return "Image files (*.jpg, *.gif, *.png)";
+		}
 	}
-
-	public String getDescription() {
-		return "Image files (*.jpg, *.gif, *.png)";
-	}
-}
