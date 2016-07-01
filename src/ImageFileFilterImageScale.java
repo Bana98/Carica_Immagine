44e25e1ff46fac5c8c10e71779d3929a4fb936d5
Albana <albana@prova.it>
2016-07-01 11:39:36 +0200

    Sistemata tabulazione

--- src/ImageFileFilterImageScale.java
+++ src/ImageFileFilterImageScale.java
@@ -13,79 +13,80 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class ImageFileFilterImageScale extends JFrame implements ActionListener {
-  Image img;
+public class ImageFileFilterImageScale extends JFrame implements ActionListener 
+{
+	Image img;
 
-  JButton getPictureButton  = new JButton("Get Picture");
+	JButton getPictureButton  = new JButton("Get Picture");
 
-  public static void main(String[] args) {
-    new ImageFileFilterImageScale();
-  }
+	public static void main(String[] args) {
+		new ImageFileFilterImageScale();
+	}
 
-  public ImageFileFilterImageScale() {
-    this.setSize(300, 300);
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public ImageFileFilterImageScale() {
+		this.setSize(300, 300);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    JPanel picPanel = new PicturePanel();
-    this.add(picPanel, BorderLayout.CENTER);
+		JPanel picPanel = new PicturePanel();
+		this.add(picPanel, BorderLayout.CENTER);
 
-    JPanel buttonPanel = new JPanel();
-    getPictureButton.addActionListener(this);
-    buttonPanel.add(getPictureButton);
-    this.add(buttonPanel, BorderLayout.SOUTH);
+		JPanel buttonPanel = new JPanel();
+		getPictureButton.addActionListener(this);
+		buttonPanel.add(getPictureButton);
+		this.add(buttonPanel, BorderLayout.SOUTH);
 
-    this.setVisible(true);
-  }
+		this.setVisible(true);
+	}
 
-  public void actionPerformed(ActionEvent e) {
-    String file = getImageFile();
-    if (file != null) {
-      Toolkit kit = Toolkit.getDefaultToolkit();
-      img = kit.getImage(file);
-//      img = img.getScaledInstance(300, -1, Image.SCALE_SMOOTH);
-//      this.repaint();
-    }
-    
-    ImageIcon icon = new ImageIcon(img);
-    
-    JLabel labelImage = new JLabel(icon);
-     add(labelImage);
-     revalidate();
-     repaint();
-  }
+	public void actionPerformed(ActionEvent e) {
+		String file = getImageFile();
+		if (file != null) {
+			Toolkit kit = Toolkit.getDefaultToolkit();
+			img = kit.getImage(file);
+			//      img = img.getScaledInstance(300, -1, Image.SCALE_SMOOTH);
+			//      this.repaint();
+		}
 
-  private String getImageFile() {
-    JFileChooser fc = new JFileChooser();
-    fc.setFileFilter(new ImageFilter());
-    int result = fc.showOpenDialog(null);
-    File file = null;
-    if (result == JFileChooser.APPROVE_OPTION) {
-      file = fc.getSelectedFile();
-      return file.getPath();
-    } else
-      return null;
+		ImageIcon icon = new ImageIcon(img);
 
-  }
+		JLabel labelImage = new JLabel(icon);
+		add(labelImage);
+		revalidate();
+		repaint();
+	}
 
-  class PicturePanel extends JPanel {
-    public void paint(Graphics g) {
-      g.drawImage(img, 0, 0, this);
-    }
-  }
+	private String getImageFile() {
+		JFileChooser fc = new JFileChooser();
+		fc.setFileFilter(new ImageFilter());
+		int result = fc.showOpenDialog(null);
+		File file = null;
+		if (result == JFileChooser.APPROVE_OPTION) {
+			file = fc.getSelectedFile();
+			return file.getPath();
+		} else
+			return null;
+
+	}
+
+	class PicturePanel extends JPanel {
+		public void paint(Graphics g) {
+			g.drawImage(img, 0, 0, this);
+		}
+	}
 
 }
 class ImageFilter extends javax.swing.filechooser.FileFilter {
-  public boolean accept(File f) {
-    if (f.isDirectory())
-      return true;
-    String name = f.getName();
-    if (name.matches(".*((.jpg)|(.gif)|(.png))"))
-      return true;
-    else
-      return false;
-  }
+	public boolean accept(File f) {
+		if (f.isDirectory())
+			return true;
+		String name = f.getName();
+		if (name.matches(".*((.jpg)|(.gif)|(.png))"))
+			return true;
+		else
+			return false;
+	}
 
-  public String getDescription() {
-    return "Image files (*.jpg, *.gif, *.png)";
-  }
+	public String getDescription() {
+		return "Image files (*.jpg, *.gif, *.png)";
+	}
 }
